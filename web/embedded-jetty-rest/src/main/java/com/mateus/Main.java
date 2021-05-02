c19f404c80404f68f9623b738aac37094d269749
Mateus Fonseca <mateuscfonseca@gmail.com>
2021-05-02 14:24:53 -0300

    atualizando para jetty 10

--- web/embedded-jetty-rest/src/main/java/com/mateus/Main.java
+++ web/embedded-jetty-rest/src/main/java/com/mateus/Main.java
@@ -15,8 +15,9 @@ public class Main {
 	var config = new App(); 
 	var appPath = config.getClass().getAnnotation(ApplicationPath.class);
 	var server = new Server(8080);
+	
 	var container = new ServletContainer(config);
-	var jerseyServlet = new ServletHolder(container);	
+	var jerseyServlet = new ServletHolder(container);
 	var context = new ServletContextHandler(server, appPath.value());
 	context.addServlet(jerseyServlet, "/*");
 	
